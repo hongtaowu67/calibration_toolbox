11c92a6742f49fafed6f80e3ee0871434a0c0eb3
Hongtao Wu <hwu67@jhu.edu>
2020-10-15 19:55:54 -0400

    add code for transforming the root of the cam to robot tool

--- src/calibration_toolbox/cam_to_world_broadcaster.py
+++ src/calibration_toolbox/cam_to_world_broadcaster.py
@@ -25,6 +25,7 @@ class Cam2WorldBroadcaster(object):
         elif camera == 'ps':
             self.cam_frame_name = "camera_rgb_optical_frame"
 
+        # root is the root of the camera tf tree
         root2cam = self.tfBuffer.lookup_transform(self.cam_frame_name, self.cam_root_name, rospy.Time(0))
         root2cam_pos_ros  = root2cam.transform.translation
         root2cam_quat_ros = root2cam.transform.rotation
@@ -48,24 +49,19 @@ class Cam2WorldBroadcaster(object):
             print(cam2ee_pose)
 
         # TODO: Multiply root2cam_pose with cam2ee_pose and translate them to quaternion and translation
-
-        cam_to_ee_pose_dict = utils.getDictFromYamlFilename(cam_to_ee_pose_file_path)
+        root2ee = np.matmul(cam2ee_pose, root2cam_pose)
         
-        self.quat = [0] * 4
-        self.quat[0] = cam_to_ee_pose_dict['cam_to_ee']['quaternion']['w']
-        self.quat[1] = cam_to_ee_pose_dict['cam_to_ee']['quaternion']['x']
-        self.quat[2] = cam_to_ee_pose_dict['cam_to_ee']['quaternion']['y']
-        self.quat[3] = cam_to_ee_pose_dict['cam_to_ee']['quaternion']['z']
-
         self.pos = [0] * 3
-        self.pos[0] = cam_to_ee_pose_dict['cam_to_ee']['translation']['x']
-        self.pos[1] = cam_to_ee_pose_dict['cam_to_ee']['translation']['y']
-        self.pos[2] = cam_to_ee_pose_dict['cam_to_ee']['translation']['z']
+        self.pos[0] = root2ee[0, -1]
+        self.pos[1] = root2ee[1, -1]
+        self.pos[2] = root2ee[2, -1]
 
+        self.quat = utils.rotm2quat(root2ee[:3, :3])
+ 
         self.t = geometry_msgs.msg.TransformStamped()
 
         self.t.header.frame_id = self.ee_frame_name
-        self.t.child_frame_id = self.cam_frame_name
+        self.t.child_frame_id = self.cam_root_name
         self.t.transform.translation.x = self.pos[0]
         self.t.transform.translation.y = self.pos[1]
         self.t.transform.translation.z = self.pos[2]
@@ -74,7 +70,7 @@ class Cam2WorldBroadcaster(object):
         self.t.transform.rotation.x = self.quat[1]
         self.t.transform.rotation.y = self.quat[2]
         self.t.transform.rotation.z = self.quat[3]
-        
+
     def cam2world_broadcaster(self):
         rate = rospy.Rate(10.0)
 
@@ -84,4 +80,19 @@ class Cam2WorldBroadcaster(object):
             self.t.header.stamp = rospy.Time.now()
             self.broadcaster.sendTransform(self.t)          
             rate.sleep()
+
+
+# cam_to_ee_pose_dict = utils.getDictFromYamlFilename(cam_to_ee_pose_file_path)
+
+# self.quat = [0] * 4
+# self.quat[0] = cam_to_ee_pose_dict['cam_to_ee']['quaternion']['w']
+# self.quat[1] = cam_to_ee_pose_dict['cam_to_ee']['quaternion']['x']
+# self.quat[2] = cam_to_ee_pose_dict['cam_to_ee']['quaternion']['y']
+# self.quat[3] = cam_to_ee_pose_dict['cam_to_ee']['quaternion']['z']
+
+# self.pos = [0] * 3
+# self.pos[0] = cam_to_ee_pose_dict['cam_to_ee']['translation']['x']
+# self.pos[1] = cam_to_ee_pose_dict['cam_to_ee']['translation']['y']
+# self.pos[2] = cam_to_ee_pose_dict['cam_to_ee']['translation']['z']
+
 
