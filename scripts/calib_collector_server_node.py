d76cac73b8b8b96c2ebb8c95c953b6a9f5e5026d
Hongtao Wu <hwu67@jhu.edu>
2021-08-13 16:40:46 +0800

    add image topic into new calib collector

--- scripts/calib_collector_server_node.py
+++ scripts/calib_collector_server_node.py
@@ -9,6 +9,7 @@ from calibration_toolbox.calib_collector import CalibrateCollector
 if __name__ == "__main__":
     target = 'chessboard'
     calib_points_file = "xxxxxx"
+    image_topic = "xxxxx"
     rospy.init_node("calib_collector_server", anonymous=True)
-    CC = CalibrateCollector(target, calib_points_file)
+    CC = CalibrateCollector(target, calib_points_file, image_topic)
     CC.run()
