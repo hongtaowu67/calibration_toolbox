1afb268037be455381e456e854993b43e4b7bf88
Hongtao Wu <hwu67@jhu.edu>
2021-08-13 15:34:55 +0800

    add run code for new collector

---
+++ scripts/calib_collector_server_node.py
@@ -0,0 +1,14 @@
+#! /usr/bin/python
+
+# Main script to run the data collector for calib_collector
+
+import rospy
+
+from calibration_toolbox.calib_collector import CalibrateCollector
+
+if __name__ == "__main__":
+    target = 'chessboard'
+    calib_points_file = "xxxxxx"
+    rospy.init_node("calib_collector_server", anonymous=True)
+    CC = CalibrateCollector(target, calib_points_file)
+    CC.run()
